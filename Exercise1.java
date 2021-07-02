d0eeb7f6adfede4e310c237cf1d715250181e41a
Cecilia Neves <81440294+cecilianeves22@users.noreply.github.com>
2021-07-02 14:06:33 -0300

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,7 +1,7 @@
 /* 1. Faça um programa que leia um valor informado pelo usuário e diga se o valor informado é positivo, 
 negativo ou zero.*/
 
-import java.util. *;
+import java.util.*;
 public class Exercise1 {
 public static void main (String [] args) {  
 Scanner sc = new Scanner (System.in);
